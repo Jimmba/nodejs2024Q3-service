ae256d43016f3b807ad51f729459b7bd1e17d251
Vitali <gimvit@gmail.com>
2024-11-06 19:34:10 +0100

    feat: import dotenv

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common';
 import { ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
 
 async function bootstrap() {
+  config();
+
+  const port = parseInt(process.env.PORT) || 4001;
   const app = await NestFactory.create(AppModule);
 
   const httpAdapterHost = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
   app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(4000);
+  console.log(port);
+  await app.listen(port);
 }
 bootstrap();
